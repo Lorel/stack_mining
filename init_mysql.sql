650c9a79b0232e14939e01905180621bf2cf5249
Aurelien <aurelien.havet@etudiant.univ-lille1.fr>
2014-11-25 16:10:51 +0100

    prevent creation in init_mysql.sql if exists

--- init_mysql.sql
+++ init_mysql.sql
@@ -1,7 +1,7 @@
-CREATE USER 'stackoverflow'@'%' IDENTIFIED BY 'stackoverflow';
-CREATE DATABASE stackoverflow;
+CREATE USER IF NOT EXISTS 'stackoverflow'@'%' IDENTIFIED BY 'stackoverflow';
+CREATE DATABASE IF NOT EXISTS stackoverflow;
 
-CREATE TABLE stackoverflow.post (
+CREATE TABLE IF NOT EXISTS stackoverflow.post (
 	id					INTEGER			PRIMARY KEY,
 	title           	VARCHAR(256)	NOT NULL,
 	body            	TEXT			NOT NULL,
@@ -9,40 +9,40 @@ CREATE TABLE stackoverflow.post (
 	creation_date		TIMESTAMP		NOT NULL
 );
 
-CREATE TABLE stackoverflow.post_answer (
+CREATE TABLE IF NOT EXISTS stackoverflow.post_answer (
 	id					INTEGER			PRIMARY KEY,
 	post_id				INTEGER			NOT NULL,
 	answer_id			INTEGER			NOT NULL,
 	score				INTEGER			NOT NULL
 );
 
-CREATE TABLE stackoverflow.stack (
+CREATE TABLE IF NOT EXISTS stackoverflow.stack (
 	id					INTEGER			PRIMARY KEY,
 	language			VARCHAR(256)	NOT NULL
 );
 
-CREATE TABLE stackoverflow.post_answer (
+CREATE TABLE IF NOT EXISTS stackoverflow.post_stack (
 	id					INTEGER			PRIMARY KEY,
 	post_id				INTEGER			NOT NULL,
 	stack_id			INTEGER			NOT NULL,
 	position			INTEGER			NOT NULL
 );
 
-CREATE TABLE stackoverflow.frame (
+CREATE TABLE IF NOT EXISTS stackoverflow.frame (
 	id					INTEGER			PRIMARY KEY,
 	method_name			VARCHAR(256)	NOT NULL,
 	file_name			VARCHAR(256)	NOT NULL,
 	line_number			INTEGER			NOT NULL
 );
 
-CREATE TABLE stackoverflow.link (
+CREATE TABLE IF NOT EXISTS stackoverflow.link (
 	id					INTEGER			PRIMARY KEY,
 	parent_frame_id		INTEGER			NOT NULL,
 	child_frame_id		INTEGER			NOT NULL,
 	next_id				INTEGER			NOT NULL
 );
 
-CREATE TABLE stackoverflow.stack_link (
+CREATE TABLE IF NOT EXISTS stackoverflow.stack_link (
 	id					INTEGER			PRIMARY KEY,
 	stack_id			INTEGER			NOT NULL,
 	link	_id			INTEGER			NOT NULL
